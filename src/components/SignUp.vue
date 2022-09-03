fcd7a7a31f3852743424b97523d81a5abfddf773
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-04 01:23:26 +0300

    created the signup page

---
+++ src/components/SignUp.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="form-container">
+    <div class="form">
+      <form action="#" class="register-form">
+        <input type="text" name="email" placeholder="Email" />
+        <input type="text" name="name" placeholder="Username" />
+
+        <input type="text" name="password" placeholder="Password" />
+        <button type="submit" class="btn btn-sign">Sign in</button>
+        <p class="message">
+          Already registered?<router-link to="/login"> Sign in</router-link>
+        </p>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+};
+</script>
