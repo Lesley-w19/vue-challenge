26397cb5ea64b4b619a7943c02ff3de8dd73460b
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-04 01:22:37 +0300

    Created a navbar pagr

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navbar">
+    <!-- LOGO -->
+    <div class="logo">Blog</div>
+    <!-- NAVIGATION MENU -->
+    <ul class="nav-links">
+      <!-- USING CHECKBOX HACK -->
+      <input type="checkbox" id="checkbox_toggle" />
+      <label for="checkbox_toggle" class="hamburger">&#9776;</label>
+      <!-- NAVIGATION MENUS -->
+      <div class="menu">
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+        <div>
+          <button class="btn">Create Blog</button>
+        </div>
+      </div>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavbarSection",
+};
+</script>
+
+<style scoped>
+img {
+  width: 30px;
+  height: 30px;
+}
+</style>
