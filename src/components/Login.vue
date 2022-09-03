f4273441404601438b2d7ee9d06bafc62575d3e4
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-04 01:22:22 +0300

    Created login page

---
+++ src/components/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="form-container">
+    <div class="form">
+      <form action="#" class="login-form">
+        <input type="text" name="username" placeholder="Username" />
+        <input type="text" name="password" placeholder="Password" />
+        <button type="submit" class="btn">Login</button>
+        <p class="message">
+          New User? <router-link to="/signup">Create an account</router-link>
+        </p>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginPage",
+};
+</script>
