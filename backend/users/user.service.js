b26d62b027c9758a3bbb70b623868f434e92ff36
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-04 14:50:40 +0300

    Created users folder and added: - service file -controller file that allows for the configuration of the user

---
+++ backend/users/user.service.js
@@ -0,0 +1,99 @@
+﻿const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+const  connection = require("_helpers/connection");
+
+// users hardcoded for simplicity, store in a db for production applications
+const users = [
+  {
+    id: 0,
+    username: "admin",
+    password: "admin",
+    firstName: "Admin",
+    lastName: "UserAdmin",
+  },
+  {
+    id: 1,
+    username: "user",
+    password: "user",
+    firstName: "Test",
+    lastName: "User",
+  },
+];
+
+module.exports = {
+  authenticate,
+  getAll,
+  addUser,
+};
+
+
+async function authenticate(usr) {
+  // const user = users.find(
+  //   (u) => u.username === username && u.password === password
+  // );
+  // const sqlQuery = "SELECT * FROM users WHERE username = ? AND password = ?;
+  const { username, password } = usr;
+  const user = await connection.query(
+    "SELECT * FROM users WHERE username = ? AND password = ?",
+    [username, password],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+      // console.log(result)
+      console.log("User found!!");
+    }
+  );
+
+  if (!user) throw "Username or password is incorrect";
+
+  // create a jwt token that is valid for 7 days
+  const secret = process.env.ACCESS_TOKEN_SECRET;
+  const token = jwt.sign({ sub: user.id }, secret, { expiresIn: "7d" });
+
+  return {
+    ...omitPassword(user),
+    token,
+  };
+}
+
+async function getAll() {
+  const sqlQuery = "SELECT * FROM users";
+  let users = await connection.query(sqlQuery, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    // console.log(result.JSON.stringify());
+  });
+  return users;
+}
+
+async function addUser(user) {
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(user.password, salt);
+
+    connection.query(
+      "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+      [user.username, user.email, hashedPassword],
+      (err, result) => {
+        // console.log(result);
+        if (err) {
+          console.log(err);
+        }
+        // result.message = "User added successfully!";
+        console.log("User added successfully...");
+      }
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// helper functions
+
+function omitPassword(user) {
+  const { password, ...userWithoutPassword } = user;
+  return userWithoutPassword;
+}
---
+++ backend/users/users.controller.js
@@ -0,0 +1,36 @@
+﻿const express = require("express");
+const router = express.Router();
+const userService = require("./user.service");
+
+// routes
+router.post("/authenticate", authenticate);
+router.get("/", getAll);
+router.post("/user", addUser);
+
+module.exports = router;
+
+function authenticate(req, res, next) {
+  userService
+    .authenticate(req.body)
+    .then((user) => {
+      console.log(user);
+    })
+    .catch(next);
+}
+
+function getAll(req, res, next) {
+  userService
+    .getAll()
+    .then((users) => {
+      console.log(users);
+
+    })
+    .catch(next);
+}
+
+function addUser(req, res, next) {
+  userService
+    .addUser(req.body)
+    .then((user) => res.json(user))
+    .catch(next);
+}
