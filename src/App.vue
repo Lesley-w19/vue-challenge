798e71168e6ed7d38424ed0574e7d8dfb5fb0f6c
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-05 12:36:34 +0300

    Added the vuex store in the main.js file

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
-  <div id="app" >
- 
-   
+  <div id="app">
     <router-view />
   </div>
 </template>
 <script>
+import { mapActions, mapGetters } from "vuex";
 export default {
   name: "AppSection",
- 
+  data() {
+    return {
+      blogs: this.blogsList,
+    };
+  },
+  methods: {
+    ...mapActions(["getBlogs", " searchBlogs"]),
+  },
+  computed: mapGetters(["blogsList"]),
+  created() {
+    this.getBlogs();
+    // console.log(this.blogsList);
+  },
 };
 </script>
