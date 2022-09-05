3cf55e610bf2f75fdc91aa0c980c1b7c65b4fa4b
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-05 12:44:01 +0300

    Blog component to display the data of an individual blog

---
+++ src/views/About.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="blogs-container">
+    <router-link to="/blogs"><h3>&larr; Back to blogs</h3></router-link>
+
+    <div class="card">
+      <img :src="blog.image" class="card__image" alt="brown couch" />
+      <div class="card__content">
+        <time datetime="2021-03-30" class="card__date">{{
+          blog.created_at
+        }}</time>
+        <h2 class="card__title">{{ blog.title }}</h2>
+        <p>{{ blog.description }}</p>
+        <h5>by {{ blog.author }}</h5>
+        <div class="btns">
+          <button class="btn btn-u">edit</button>
+          <button class="btn btn-d" @click="delBlog(blog.id)">Delete</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions, mapGetters } from "vuex";
+export default {
+  name: "SingleBlog",
+  data() {
+    return {
+      blog_id: this.$route.params.id,
+    };
+  },
+  methods: {
+    ...mapActions(["getBlog", "deleteBlog"]),
+    delBlog(id) {
+      this.deleteBlog(id);
+    },
+  },
+  computed: mapGetters(["blog"]),
+  created() {
+    this.getBlog(this.blog_id);
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.blogs-container {
+  padding: 20px 40px;
+  h3 {
+    text-align: start;
+    margin-left: 40px;
+    color: #0868a5;
+    text-decoration: underline;
+  }
+}
+@mixin mediaBig {
+  @media (min-width: 576px) {
+    @content;
+  }
+}
+
+@include mediaBig {
+  :root {
+    --font-size-title: 40px;
+  }
+}
+
+.card {
+  max-width: 960px;
+  border-radius: 5px;
+  margin: 20px auto;
+  margin-top: 0;
+
+  @include mediaBig {
+    flex-direction: row;
+    align-items: center;
+    margin: 40px;
+    padding: 32px;
+  }
+
+  .card__image {
+    width: 100%;
+    height: 200px;
+    object-fit: cover;
+    margin-bottom: 18px;
+
+    @include mediaBig {
+      width: 45%;
+      max-height: none;
+      min-height: 400px;
+      margin-bottom: 0;
+    }
+  }
+
+  .card__content {
+    padding: 10px 20px;
+
+    @include mediaBig {
+      width: 55%;
+      padding-left: 40px;
+    }
+  }
+
+  .card__date {
+    display: block;
+    text-transform: uppercase;
+    color: var(--color-text);
+    margin-bottom: 10px;
+
+    @include mediaBig {
+      margin-bottom: 20px;
+      font-weight: lighter;
+      letter-spacing: 1px;
+      text-align: right;
+    }
+  }
+
+  .card__title {
+    margin: 30px auto;
+    font-size: 30px;
+    padding: 10px 20px;
+    text-transform: uppercase;
+  }
+
+  p,
+  h6 {
+    margin: 10px 0;
+    letter-spacing: 1px;
+  }
+  .btns {
+    width: 100%;
+    padding: 10px 0;
+    button {
+      padding: 10px 30px;
+      border: none;
+      border-radius: 5px;
+      color: #fff;
+      text-transform: uppercase;
+      margin: 1px 20px 0 0;
+      cursor: pointer;
+    }
+    ¯ .btn-d {
+      background-color: #ff2600;
+    }
+    .btn-u {
+      background-color: #0868a5;
+    }
+  }
+}
+</style>
