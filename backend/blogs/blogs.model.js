f55faa9ed49cf12af206effce19dd6d6e615f050
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-06 09:46:41 +0300

    Commented all console.log

--- backend/blogs/blogs.model.js
+++ backend/blogs/blogs.model.js
@@ -12,23 +12,23 @@ const Blog = function (blog) {
 Blog.create = (newBlog, result) => {
   sql.query("INSERT INTO blogs SET ?", newBlog, (err, res) => {
     if (err) {
-      console.log("error: ", err);
+      // console.log("error: ", err);
       result(err, null);
       return;
     }
-    console.log("created Blog: ", { id: res.insertId, ...newBlog });
+    // console.log("created Blog: ", { id: res.insertId, ...newBlog });
     result(null, { id: res.insertId, ...newBlog });
   });
 };
 Blog.findById = (id, result) => {
   sql.query(`SELECT * FROM blogs WHERE id = ${id}`, (err, res) => {
     if (err) {
-      console.log("error: ", err);
+      // console.log("error: ", err);
       result(err, null);
       return;
     }
     if (res.length) {
-      console.log("found Blog: ", res[0]);
+      // console.log("found Blog: ", res[0]);
       result(null, res[0]);
       return;
     }
@@ -47,7 +47,7 @@ Blog.getAll = (title, result) => {
       result(null, err);
       return;
     }
-    console.log("blogs: ", res);
+    // console.log("blogs: ", res);
     result(null, res);
   });
 };
@@ -63,12 +63,13 @@ Blog.getAll = (title, result) => {
 //   });
 // };
 Blog.updateById = (id, blog, result) => {
+  console.log(blog);
   sql.query(
-    "UPDATE blogs SET title = ?, description = ?, published = ? WHERE id = ?",
-    [blog.title, blog.description, blog.published, id],
+    "UPDATE blogs SET title = ?, description = ?, author = ?,image = ?, published = ? WHERE id = ?",
+    [blog.title, blog.description, blog.author, blog.image, blog.published, id],
     (err, res) => {
       if (err) {
-        console.log("error: ", err);
+        // console.log("error: ", err.message);
         result(null, err);
         return;
       }
@@ -77,7 +78,7 @@ Blog.updateById = (id, blog, result) => {
         result({ kind: "not_found" }, null);
         return;
       }
-      console.log("updated Blog: ", { id: id, ...blog });
+      // console.log("updated Blog: ", { id: id, ...blog });
       result(null, { id: id, ...blog });
     }
   );
@@ -85,7 +86,7 @@ Blog.updateById = (id, blog, result) => {
 Blog.remove = (id, result) => {
   sql.query("DELETE FROM blogs WHERE id = ?", id, (err, res) => {
     if (err) {
-      console.log("error: ", err);
+      // console.log("error: ", err);
       result(null, err);
       return;
     }
@@ -94,11 +95,10 @@ Blog.remove = (id, result) => {
       result({ kind: "not_found" }, null);
       return;
     }
-    console.log("deleted blog with id: ", id);
+    // console.log("deleted blog with id: ", id);
     result(null, res);
   });
 };
-
 
 // Blog.removeAll = (result) => {
 //   sql.query("DELETE FROM blogs", (err, res) => {
